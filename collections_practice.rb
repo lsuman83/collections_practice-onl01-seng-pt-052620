c82447dd6e6a6cbbd26dfd79783d9c1893381882
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 03:37:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,26 +1,44 @@
+#1
 def sort_array_asc (array)
   array.sort
 end
 
+#2
 def sort_array_desc (array)
   array.sort {|a, b| b <=> a}
 end
 
+#3
 def sort_array_char_count (array)
   array.sort_by {|word| word.length}
 end
 
+#4
 def swap_elements (array)
   array.sort {word[1] <=> word[2]
 end
 
+#5
 def reverse_array (array)
   array.reverse
 end
 
+#6
 def kesha_maker (array)
   
 end
 
+#7
 def find_a (array)
   
+end
+
+#8
+def sum_array (array)
+  
+end
+
+#9
+def add_s (array)
+  
+end
