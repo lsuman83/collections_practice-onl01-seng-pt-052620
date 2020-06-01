080ff6bc4c24544ff9ea3496385b7ca7b910f095
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 06:01:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+#1
+def sort_array_asc (array)
+  array.sort
+end
 
+#2
+def sort_array_desc (array)
+  array.sort {|a, b| b <=> a}
+end
+
+#3
+def sort_array_char_count (array)
+  array.sort_by {|word| word.length}
+end
+
+#4
+def swap_elements (array)
+  array.sort_by {|x| x[-1]}
+end
+
+#5
+def reverse_array (array)
+  array.reverse
+end
+
+#6
+def kesha_maker (array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array << word
+  end
+  new_array
+end
+
+#7
+def find_a (array)
+  array.select {|word| word.start_with? ("a")}
+end
+
+#8
+def sum_array (array)
+  array.inject {|sum, i| sum + i}
+end
+
+#9
+def add_s (array)
+  
+  index = 0
+  new_array = []
+  array.each do |x| 
+    next if x == 'feet'
+    x.concat("s")
+  end
+  array
+end
